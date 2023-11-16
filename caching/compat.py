aa82771a50e212dbbf5100f028374e532ff64665
carole <carole.sartori@onysos.fr>
2023-11-16 13:31:38 +0100

    fix (DEFAULT_TIMEOUT): Django DEFAULT_TIMEOUT is an object, int value is required.
    - set DEFAULT_TIMEOUT value for the Django version 1.6..
    - bump version

--- caching/compat.py
+++ caching/compat.py
@@ -7,7 +7,7 @@ __all__ = ['DEFAULT_TIMEOUT', 'FOREVER']
 
 if django.VERSION[:2] >= (1, 6):
     from django.core.cache.backends.base import DEFAULT_TIMEOUT as DJANGO_DEFAULT_TIMEOUT
-    DEFAULT_TIMEOUT = DJANGO_DEFAULT_TIMEOUT
+    DEFAULT_TIMEOUT = 300 if django.VERSION[:2] == (1, 6) else DJANGO_DEFAULT_TIMEOUT
     FOREVER = None
 else:
     DEFAULT_TIMEOUT = None
